dfb3a9a1481f417f8b249974dd89205e5999c674
root <root@vps341517.ovh.net>
2016-11-24 21:18:24 +0100

    updates

---
+++ x86_64-Linux/CMakeFiles/pool_frontend_zcash.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "protocol.pb.cc"
+  "protocol.pb.h"
+  "CMakeFiles/pool_frontend_zcash.dir/main.cpp.o"
+  "CMakeFiles/pool_frontend_zcash.dir/zcashpool.cpp.o"
+  "CMakeFiles/pool_frontend_zcash.dir/zmtp.cpp.o"
+  "CMakeFiles/pool_frontend_zcash.dir/equihash_original.cpp.o"
+  "CMakeFiles/pool_frontend_zcash.dir/stratum.cpp.o"
+  "CMakeFiles/pool_frontend_zcash.dir/protocol.pb.cc.o"
+  "pool_frontend_zcash.pdb"
+  "pool_frontend_zcash"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pool_frontend_zcash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
